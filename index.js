5d79dd414d805ec90e83a6ddf9377e1543ad8431
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2019-02-12 10:38:13 +0200

    Fixed grid issue

--- index.js
+++
@@ -1,7 +0,0 @@
-import html from "./index.pug"
-// Import styles
-import style from "./styles/main.sass";
-// Import scripts
-import script from "./scripts/plugin.js";
-
-console.log('all files loaded');
